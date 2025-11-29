fa8e5de21415b8850130081f75056688731fed47
Teterschaft <gofla2242@tukorea.ac.kr>
2025-11-29 14:43:44 +0900

    feat: EAggCollisionShape Enum 정의
    물리 Shape의 타입을 구분하는 열거형

---
+++ Mundi/Source/Runtime/Physics/ShapeElem.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+#include "UEContainer.h"
+
+// Shape 타입 열거형
+enum class EAggCollisionShape : uint8
+{
+    Sphere,
+    Box,
+    Sphyl,      // Capsule
+    Convex,
+    Unknown
+};
