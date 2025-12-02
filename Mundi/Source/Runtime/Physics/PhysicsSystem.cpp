d752922d737d44538d01340c53e2470f853e0b19
fuenell <wlscjf1404@gmail.com>
2025-12-03 00:04:48 +0900

    feat: 자동차 이동 로직 추가 (아주 천천히 움직임)

--- Mundi/Source/Runtime/Physics/PhysicsSystem.cpp
+++ Mundi/Source/Runtime/Physics/PhysicsSystem.cpp
@@ -31,6 +31,8 @@ void FPhysicsSystem::Initialize()
         UE_LOG("[error][FPhysicsSystem]: PxInitVehicleSDK Error");
         return;
     }
+    PxVehicleSetBasisVectors(PxVec3(0, 1, 0), PxVec3(0, 0, 1));
+    PxVehicleSetUpdateMode(PxVehicleUpdateMode::eACCELERATION);
 
     // Extensions 초기화 (Joint, Serialization 등 확장 기능 사용을 위해 필수)
     if (!PxInitExtensions(*mPhysics, mPvd))
